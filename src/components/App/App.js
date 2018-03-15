65670a083293ab7cac2eeb5adf026a642132d08c
Christiana Routon <christiana.routon@gmail.com>
2018-03-15 11:56:08 -0500

    stars with diameter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -7,14 +6,36 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      starList: ['Gacrux', 'Hadar', 'Fomalhaut'],
+      starList: [
+        {name:'Gacrux',
+        diameter: 117000000},
+        {name:'Fomalhaut',
+        diameter: 4},
+        {name:'Hadar',
+        diameter: 13000000}
+      ],
     }
   }
 
   render() {
-    const starListItemArray = this.state.starList.map((star, i) => <li key={i}>{star}</li>);
+    // function objectToStarName(eachStarObject){
+    //   return eachStarObject.name;
+    // }
+    // function objectToStarDiameter(eachStarObject){
+    //   return eachStarObject.diameter;
+    // }
+    // starNameArray.forEach(star => {
+    //   const newName = objectToStarName(star);
+    // })
+    // starDiameterArray.forEach(star => {
+    //   const newDiameter = objectToStarDiameter(star);
+    // })
+    // const starListItemArray = this.state.starList.map((star, i) => <li key={i}>{star}</li>);
     return (
-        <ul>{starListItemArray}</ul>
+         <div>
+         {this.state.starList.map((starList, i) => <p key={i}>{starList.name} has a diameter of {starList.diameter}</p>)}
+
+         </div>
     );
   }
 }
